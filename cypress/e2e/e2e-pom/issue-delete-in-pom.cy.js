38e4d540bad2019e6f8e8b181eecc4ca0b6625ec
YlleTyrk <tyrkylle@gmail.com>
2024-02-23 17:19:22 +0200

    week2

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -1,25 +1,47 @@
 /**
  * This is an example file and approach for POM in Cypress
  */
-import IssueModal from "../../pages/IssueModal";
+import IssueModal from '../../pages/IssueModal';
 
 describe('Issue delete', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
-    cy.contains(issueTitle).click();
-    });
+    cy.url()
+      .should('eq', `${Cypress.env('baseUrl')}project/board`)
+      .then((url) => {
+        //open issue detail modal with title from line 16
+        cy.contains(issueTitle).click();
+      });
   });
 
   //issue title, that we are testing with, saved into variable
   const issueTitle = 'This is an issue of type: Task.';
+  const issueDetailModal = '[data-testid="modal:issue-details"]';
+  const deleteButton = '[data-testid="icon:trash"]';
+  const confirmationPopup = '[data-testid="modal:confirm"]';
+  const deleteButtonName = 'Delete issue';
+  
 
   it('Should delete issue successfully', () => {
-    //add steps to delete issue
+    cy.get(issueDetailModal).should('be.visible');
+    cy.get(deleteButton).click();
+    cy.get(confirmationPopup).should('be.visible');
+    cy.get(confirmationPopup).within(() => {
+      cy.contains(deleteButtonName).click();
+     cy.get(confirmationPopup).should('not.exist');
+     
+      
+    });
   });
 
   it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
-  });
+    cy.get(issueDetailModal).should('be.visible');
+    cy.get(deleteButton).click();
+    cy.get(confirmationPopup).should('be.visible');
+    cy.get(confirmationPopup).within(() => {
+      cy.contains(cancelDeletionButtonName).click();
+      cy.get(confirmationPopup).should('not.exist')
+
+    })
+  })
 });
